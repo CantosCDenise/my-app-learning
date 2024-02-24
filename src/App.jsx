8faf052ccd693178e608a14e7fa46870cd95835f
CantosCDenise <denisecantos2905@gmail.com>
2024-02-24 14:55:55 +0100

    video react router hasta min 26

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,22 @@
-import { Age } from "./Age";
-import { AlertClock } from "./AlertClock";
-import { Clock } from "./Clock";
 import { Container } from "./Container";
-import { Counter } from "./Counter";
-import { Hello } from "./Hello";
-import { Message } from "./Message";
-import { MouseClicker } from "./MouseClicker";
-import { MyForm } from "./MyForm";
-import { MyList } from "./MyList";
 import { MyUncontrolledForm } from "./MyUncontrolledForm";
 import { Welcome } from "./Welcome";
-import { LanguageContext } from "./LanguageContext";
-import { useState } from "react";
-import { FormCheck } from "./Checkpoint/FormCheck";
-import { Toggle } from "./Checkpoint/Toggle";
-import { TodoList } from "./TodoList";
-import { GithubUser } from "./GithubUser";
-import { HookCounter } from "./HookCounter";
-import { useCounter } from "./UseCounter";
+import { Route, Routes } from "react-router-dom";
 
 export function App(){
-    function handleShowTime(){
-        const now = new Date()
-
-        alert(now.toLocaleTimeString())
-    }
-
-    const [language, setLanguage] = useState("en")
-
-    function handleSetLanguage(language){
-        setLanguage(language)
-    }
-
-    const {counter, onIncrement} = useCounter()
-    const [username, setUsername] = useState("")
     return (
-    <div>
-        <button onClick={() => handleSetLanguage("it")}>IT</button>
-        <button onClick={() => handleSetLanguage("en")} >EN</button>
-
-        <Container title={<h1>My Awesome App</h1>}>
-            <LanguageContext.Provider value={language}>
-            <Hello/>
-            <Hello/>
-            <Hello/>
-            <input value={username} onChange={(e) => setUsername(e.target.value)}/>
-            <GithubUser username="cantoscdenise"/>
-            <Message/>
-            <Welcome name="Denise" age={24}/>
-            <AlertClock onClick={handleShowTime}/>
-            <Counter/>
-            <Clock/>
-            <MouseClicker/>
-            <MyForm/>
-            <MyList items={[
-                { id: 1, name: 'Jane', age: 20},
-                { id: 2, name: 'John', age: 23},
-                { id: 3, name: 'Billy', age: 40},
-                { id: 4, name: 'Kate', age: 26},
-                { id: 5, name: 'Jane', age: 30},
-            ]}/>
-            <Toggle/>
-            <FormCheck/>
-            <TodoList/>
-            </LanguageContext.Provider>
-            <HookCounter/>
-            <button onClick={onIncrement}>{counter}</button>
-        </Container>
-    </div>
+    <Container title="My Awesome App">
+        <Routes>
+            <Route path="/" element={<Welcome age="24"/>} />
+            <Route path="/:name" element={<Welcome age="24"/>} />
+            <Route path="form" element={<MyUncontrolledForm />} />
+        </Routes>
+    </Container>        
     )
 }
 
+
+//a Routes component is something that is getting imported from react router dom
 
 //al my application is now a descendat of the language-context provider. We can hold any value within a provider. 
     //We always need to pass a value to the value prop of a provider (the default value will be used another way)
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,13 +1,28 @@
-export function Welcome({ name, age }){
-const WelcomeStyle = {
-    backgroundColor: name ? 'greenyellow' : 'darkgray',
-    color: '#333'
-}
+import { Link, useNavigate, useParams } from "react-router-dom"
+
+export function Welcome({ age }){
+
+    const { name = "World" } = useParams()
+
+    const WelcomeStyle = {
+        backgroundColor: name ? 'greenyellow' : 'darkgray',
+        color: '#333'
+    }
+
+    const navigate = useNavigate()
+
+    function handleFormButtonClick(){
+        navigate('/form')
+    }
 
     return (
     <div style={WelcomeStyle}>
         <strong> Welcome, {name}!</strong>
         <p>Your age is {age}</p>
+        {/* <Link to="/login">Login to the app</Link> */}
+        <div>
+            <button onClick={handleFormButtonClick}>Enter the app</button>
+        </div>
     </div>
         )
 }
